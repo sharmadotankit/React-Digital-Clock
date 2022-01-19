f8ad39cb51168ebaeac00e8c88d9aeabe2e4c455
sharmadotankit <sharma.ankit.kamal@gmail.com>
2022-01-19 19:20:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ class App extends Component {
     super();
     this.state = {
      date: new Date()
-    }
-  }
+   }
+ }
 
 
-componentDidMount() {
+ componentDidMount() {
   this.interval = setInterval(() => this.setState({ date: new Date()}), 999);
+  let day = document.getElementById(this.state.date.getDay());
+  day.style.color='red';
+
 }
 componentWillUnmount() {
   clearInterval(this.interval);
@@ -19,69 +22,63 @@ componentWillUnmount() {
 
 
 
-  render(){
-    const presentDate = this.state.date;
-    let dayId = presentDate.getDay();
+render(){
+  const presentDate = this.state.date;
+  
+  return (
+    <div className="App">
+    <h1 id = "clock-title"> Digital Clock</h1>
+    <div className="mainContainer">
 
-    let day = document.getElementById(dayId);
-    console.log(day)
+    <div className="dateContainer">
+    <div className="dateTitleContainer">
+    <p>Date</p>
+    <p>Month</p>
+    <p>Year</p>
+    </div>
 
-    // day.style.color='red';
-    
-    return (
-      <div className="App">
-      <h1 id = "clock-title"> Digital Clock</h1>
-        <div className="mainContainer">
+    <div className="dateValueContainer">
+    <h1>{ presentDate.getDate()}</h1>
 
-          <div className="dateContainer">
-              <div className="dateTitleContainer">
-                <p>Date</p>
-                <p>Month</p>
-                <p>Year</p>
-              </div>
-
-            <div className="dateValueContainer">
-              <h1>{ presentDate.getDate()}</h1>
-
-              <h1>/</h1>
-              <h1>{ ( (presentDate.getMonth()+1) < 10 ? "0"+(presentDate.getMonth()+1).toString() :(presentDate.getMonth()+1) )}</h1>
-              <h1>/</h1>
-              <h1>{presentDate.getFullYear()}</h1>
-            </div>
-              
-
-          </div>
-
-          <div className="timeContainer">
-
-            <div className="timeTitleContainer">
-              <p>Hour</p>
-              <p>Minute</p>
-              <p>Second</p>
-            </div>
-
-            <div className="timeValueContainer">
-              <h1> {presentDate.getHours()} </h1>
-              <h1>:</h1>
-              <h1>{ ( (presentDate.getMinutes()+1) < 10 ? "0"+(presentDate.getMinutes()+1).toString() :(presentDate.getMinutes()+1) ) }</h1>
-              <h1>:</h1>
-              <h1>{ ( (presentDate.getSeconds()+1) < 10 ? "0"+(presentDate.getSeconds()+1).toString() :(presentDate.getSeconds()+1) ) }</h1>
-            </div>
-
-          </div>
+    <h1>/</h1>
+    <h1>{ ( (presentDate.getMonth()+1) < 10 ? "0"+(presentDate.getMonth()+1).toString() :(presentDate.getMonth()+1) )}</h1>
+    <h1>/</h1>
+    <h1>{presentDate.getFullYear()}</h1>
+    </div>
 
 
-          <div className="dayContainer">
+    </div>
 
-            <p id="0"> Sun</p>
-            <p id="1"> Mon</p>
-            <p id="2"> Tue</p>
-            <p id="3"> Wed</p>
-            <p id="4"> Thur</p>
-            <p id="5"> Fri</p>
-            <p id="6"> Sat</p>
-                  
-           </div>
+    <div className="timeContainer">
+
+    <div className="timeTitleContainer">
+    <p>Hour</p>
+    <p>Minute</p>
+    <p>Second</p>
+    </div>
+
+    <div className="timeValueContainer">
+    <h1> {presentDate.getHours()} </h1>
+    <h1>:</h1>
+    <h1>{ ( (presentDate.getMinutes()+1) < 10 ? "0"+(presentDate.getMinutes()+1).toString() :(presentDate.getMinutes()+1) ) }</h1>
+    <h1>:</h1>
+    <h1>{ ( (presentDate.getSeconds()+1) < 10 ? "0"+(presentDate.getSeconds()+1).toString() :(presentDate.getSeconds()+1) ) }</h1>
+    </div>
+
+    </div>
+
+
+    <div className="dayContainer">
+
+    <p id="0"> Sun</p>
+    <p id="1"> Mon</p>
+    <p id="2"> Tue</p>
+    <p id="3"> Wed</p>
+    <p id="4"> Thur</p>
+    <p id="5"> Fri</p>
+    <p id="6"> Sat</p>
+
+    </div>
 
           {/*
             <div className="bottomContainer">
@@ -97,11 +94,11 @@ componentWillUnmount() {
 
           </div>
           */
-          }
+        }
         </div>
-      </div>
-      );
-  }
+        </div>
+        );
+}
 }
 
 export default App;
